2ca949aaad6b7e350b3691d50b900de3f687de5f
Nacho Vazquez <nachovc1410@gmail.com>
2020-02-25 13:32:14 -0300

    initial commit

---
+++ apps/airline-web-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'composite-shell-variation-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'airline-web-app';
+}
